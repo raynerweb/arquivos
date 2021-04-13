5be7a0ec663ca0c6fd877763d3ad2ebd356bf250
Ráyner Carvalho Lima <raynerweb@gmail.com>
2021-04-13 17:34:44 -0300

    removendo entidade tipo_arquivo do modelo de negocios

--- src/main/resources/db/migration/h2/V1.1__insert.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO ARQUIVOS.TB_TIPO_ARQUIVO (COD_TIPO_ARQUIVO, TXT_DESCRICAO, TXT_CAMINHO_ARMAZENAMENTO, TXT_CONTENT_TYPE)
-VALUES (1, 'Arquivo de Imagem JPG', '/tmp/image', 'image/jpeg'),
-       (2, 'Arquivo de Imagem PNG', '/tmp/image', 'image/png'),
-       (3, 'Arquivo de Imagem PNG', '/tmp/image', 'video/x-msvideo');
