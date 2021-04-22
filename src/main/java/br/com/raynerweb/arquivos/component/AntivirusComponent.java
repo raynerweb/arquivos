f06dd8675ba880462fede561c4932634f09ed2c4
Ráyner Carvalho Lima <raynerweb@gmail.com>
2021-04-22 20:11:20 -0300

    timeout 60

--- src/main/java/br/com/raynerweb/arquivos/component/AntivirusComponent.java
+++ src/main/java/br/com/raynerweb/arquivos/component/AntivirusComponent.java
@@ -16,7 +16,7 @@ public class AntivirusComponent {
 
     public void verifiy(byte[] file) {
         try {
-            ClamAVClient a = new ClamAVClient("127.0.0.1", 3310, 20000);
+            ClamAVClient a = new ClamAVClient("127.0.0.1", 3310, 60000);
             byte[] r = a.scan(file);
             if (!ClamAVClient.isCleanReply(r)) {
                 LOG.error("Virus Detectado");
